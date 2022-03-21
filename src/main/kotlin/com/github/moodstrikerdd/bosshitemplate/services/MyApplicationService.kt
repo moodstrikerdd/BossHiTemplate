8dd81aa054250a0fbf3bf10bb2b45370f8d0e74f
GitHub Action <action@github.com>
2022-03-21 02:18:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/moodstrikerdd/bosshitemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.moodstrikerdd.bosshitemplate.services
+
+import com.github.moodstrikerdd.bosshitemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
